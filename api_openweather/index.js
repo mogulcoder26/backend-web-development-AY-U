b16666ccd3b44344f2b1d4a7b356a06e90740bf2
Soubhik <b422056@iiit-bh.ac.in>
2023-03-05 19:28:38 +0530

    final commit

--- api_openweather/index.js
+++ api_openweather/index.js
@@ -1,33 +1,49 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const https = require('https');
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=odisha,ind&appid=61841bb48d2a721340b05871a8857a0c&units=metric";
-    https.get(url, function (response) {
-        console.log(req);
-        console.log(req.statusCode);
+for(var i=0;i<3;i++){
 
-        response.on('data', function (data) {
-            const openWeatherMap = JSON.parse(data);
-            // console.log(openWeatherMap);
-
-            // console.log(JSON.stringify(object));
-            const temp = openWeatherMap.main.temp;
-            // console.log(temp);
-            // console.log(openWeatherMap.weather[0].description);
-            // res.send("<h1>Temperature today at "+openWeatherMap.name+" is " +temp+" degree celcius.</h1>");
-            // res.send("hello");
-
-            res.write("<h1>Temperature today at "+openWeatherMap.name+" is " +temp+" degree celcius.</h1>");
-            const weatherCode=openWeatherMap.weather[0].icon;
-            const img_link="http://openweathermap.org/img/wn/"+ weatherCode +"@2x.png";
-            res.write("<img src="+img_link+">");
-            res.send();
-        });
-     });
-
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/openweathermap.html");
 });
+
+app.post("/",function(req,res){
+   console.log(req.body.cityName);
+
+   const url = "https://api.openweathermap.org/data/2.5/weather?q="+req.body.cityName+"&appid=61841bb48d2a721340b05871a8857a0c&units=metric";
+   https.get(url, function (response) {
+       console.log(req);
+       console.log(req.statusCode);
+
+       response.on('data', function (data) {
+           const openWeatherMap = JSON.parse(data);
+           // console.log(openWeatherMap);
+
+           // console.log(JSON.stringify(object));
+           const temp = openWeatherMap.main.temp;
+           // console.log(temp);
+           // console.log(openWeatherMap.weather[0].description);
+           // res.send("<h1>Temperature today at "+openWeatherMap.name+" is " +temp+" degree celcius.</h1>");
+           // res.send("hello");
+
+           res.write("<h1>Temperature today at "+openWeatherMap.name+" is " +temp+" degree celcius.</h1>");
+           const weatherCode=openWeatherMap.weather[0].icon;
+           const img_link="http://openweathermap.org/img/wn/"+ weatherCode +"@2x.png";
+           res.write("<img src="+img_link+">");
+           res.send();
+       });
+    });
+});
+// app.get("/", function (req, res) {
+
+
+// });
+
+}
+
 
 
 app.listen('3000', function () {
